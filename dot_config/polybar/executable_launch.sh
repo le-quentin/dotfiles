f867aad3a98d61ee1f3a01ec43363de55f455ac6
Quentin Bonnet <quentin.bonnet.work@gmail.com>
2022-10-18 13:49:01 +0200

    Add polybar themes, and customise them to work on dual screen. Use hack theme

--- dot_config/polybar/executable_launch.sh
+++ dot_config/polybar/executable_launch.sh
@@ -1,18 +1,95 @@
 #!/usr/bin/env bash
 
-# Terminate already running bar instances
-# If all your bars have ipc enabled, you can use 
-polybar-msg cmd quit
-# Otherwise you can use the nuclear option:
-# killall -q polybar
+dir="$HOME/.config/polybar"
+themes=(`ls --hide="launch.sh" $dir`)
 
-echo "---" | tee -a /tmp/polybar1.log /tmp/polybar2.log
-if type "xrandr"; then
-  for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
-    MONITOR=$m polybar --reload example 2>&1 | tee -a /tmp/polybar.log & disown
-  done
+launch_bar_base() {
+	# Launch the bar
+	if [[ "$style" == "hack" || "$style" == "cuts" ]]; then
+		MONITOR=$m DIR=$dir STYLE=$style polybar -q top -c "$dir/$style/config.ini" &
+		MONITOR=$m DIR=$dir STYLE=$style polybar -q bottom -c "$dir/$style/config.ini" &
+	elif [[ "$style" == "pwidgets" ]]; then
+		bash "$dir"/pwidgets/launch.sh --main
+	else
+		MONITOR=$m DIR=$dir STYLE=$style polybar -q main -c "$dir/$style/config.ini" &	
+	fi
+}
+
+launch_bar() {
+	# Terminate already running bar instances
+	killall -q polybar
+
+	# Wait until the processes have been shut down
+	while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
+
+	# Run the bar on each screen
+	echo "---" | tee -a /tmp/polybar1.log /tmp/polybar2.log
+	if type "xrandr"; then
+	  for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
+	    MONITOR=$m DIR=$dir STYLE=$style launch_bar_base
+	  done
+	else
+	  launch_bar_base
+	fi
+
+	echo "Bars launched..."
+}
+
+if [[ "$1" == "--material" ]]; then
+	style="material"
+	launch_bar
+
+elif [[ "$1" == "--shades" ]]; then
+	style="shades"
+	launch_bar
+
+elif [[ "$1" == "--hack" ]]; then
+	style="hack"
+	launch_bar
+
+elif [[ "$1" == "--docky" ]]; then
+	style="docky"
+	launch_bar
+
+elif [[ "$1" == "--cuts" ]]; then
+	style="cuts"
+	launch_bar
+
+elif [[ "$1" == "--shapes" ]]; then
+	style="shapes"
+	launch_bar
+
+elif [[ "$1" == "--grayblocks" ]]; then
+	style="grayblocks"
+	launch_bar
+
+elif [[ "$1" == "--blocks" ]]; then
+	style="blocks"
+	launch_bar
+
+elif [[ "$1" == "--colorblocks" ]]; then
+	style="colorblocks"
+	launch_bar
+
+elif [[ "$1" == "--forest" ]]; then
+	style="forest"
+	launch_bar
+
+elif [[ "$1" == "--pwidgets" ]]; then
+	style="pwidgets"
+	launch_bar
+
+elif [[ "$1" == "--panels" ]]; then
+	style="panels"
+	launch_bar
+
 else
-  polybar --reload example 2>&1 | tee -a /tmp/polybar.log & disown
+	cat <<- EOF
+	Usage : launch.sh --theme
+		
+	Available Themes :
+	--blocks    --colorblocks    --cuts      --docky
+	--forest    --grayblocks     --hack      --material
+	--panels    --pwidgets       --shades    --shapes
+	EOF
 fi
-
-echo "Bars launched..."
