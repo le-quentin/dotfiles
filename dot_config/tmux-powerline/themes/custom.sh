6234916e75d403dd9ee495c740704e550a12b96d
Quentin Bonnet <quentin.bonnet.work@gmail.com>
2022-10-20 18:24:39 +0200

    First tmux-powerline theme, optimised for ocean-next alacritty theme

--- dot_config/tmux-powerline/themes/custom.sh
+++ dot_config/tmux-powerline/themes/custom.sh
@@ -12,8 +12,15 @@ else
 	TMUX_POWERLINE_SEPARATOR_RIGHT_THIN="❯"
 fi
 
-TMUX_POWERLINE_DEFAULT_BACKGROUND_COLOR=${TMUX_POWERLINE_DEFAULT_BACKGROUND_COLOR:-'0'}
-TMUX_POWERLINE_DEFAULT_FOREGROUND_COLOR=${TMUX_POWERLINE_DEFAULT_FOREGROUND_COLOR:-'255'}
+TMUX_POWERLINE_DEFAULT_BACKGROUND_COLOR='colour8'
+TMUX_POWERLINE_DEFAULT_FOREGROUND_COLOR='colour15'
+
+tmux set-option -g status-bg $TMUX_POWERLINE_DEFAULT_BACKGROUND_COLOR
+tmux set-option -g status-fg $TMUX_POWERLINE_DEFAULT_FOREGROUND_COLOR
+tmux set-option -g window-status-bell-style default
+tmux set-option -g window-status-activity-style default
+tmux set-option -g window-status-current-style bg=colour7,fg=colour0
+tmux set-option -g window-status-separator '  '
 
 TMUX_POWERLINE_DEFAULT_LEFTSIDE_SEPARATOR=${TMUX_POWERLINE_DEFAULT_LEFTSIDE_SEPARATOR:-$TMUX_POWERLINE_SEPARATOR_RIGHT_BOLD}
 TMUX_POWERLINE_DEFAULT_RIGHTSIDE_SEPARATOR=${TMUX_POWERLINE_DEFAULT_RIGHTSIDE_SEPARATOR:-$TMUX_POWERLINE_SEPARATOR_LEFT_BOLD}
@@ -52,13 +59,13 @@ fi
 
 if [ -z $TMUX_POWERLINE_LEFT_STATUS_SEGMENTS ]; then
 	TMUX_POWERLINE_LEFT_STATUS_SEGMENTS=(
-		"tmux_session_info 148 234" \
-		"hostname 33 0" \
+		"tmux_session_info 8 15" \
+		"hostname 4 0" \
 		#"ifstat 30 255" \
 		#"ifstat_sys 30 255" \
 		#"lan_ip 24 255 ${TMUX_POWERLINE_SEPARATOR_RIGHT_THIN}" \
 		#"wan_ip 24 255" \
-		"vcs_branch 29 88" \
+		"vcs_branch 8 2" \
 		#"vcs_compare 60 255" \
 		#"vcs_staged 64 255" \
 		#"vcs_modified 9 255" \
@@ -69,20 +76,20 @@ fi
 if [ -z $TMUX_POWERLINE_RIGHT_STATUS_SEGMENTS ]; then
 	TMUX_POWERLINE_RIGHT_STATUS_SEGMENTS=(
 		#"earthquake 3 0" \
-		"pwd 89 211" \
+		"pwd 4 0" \
 		#"macos_notification_count 29 255" \
 		#"mailcount 9 255" \
-		"now_playing 234 37" \
+		#"now_playing 234 37" \
 		#"cpu 240 136" \
 		#"load 237 167" \
 		#"tmux_mem_cpu_load 234 136" \
-		"battery 137 127" \
-		"weather 37 255" \
+		"battery 8 15" \
+		#"weather 37 255" \
 		#"rainbarf 0 ${TMUX_POWERLINE_DEFAULT_FOREGROUND_COLOR}" \
 		#"xkb_layout 125 117" \
-		"date_day 235 136" \
-		"date 235 136 ${TMUX_POWERLINE_SEPARATOR_LEFT_THIN}" \
-		"time 235 136 ${TMUX_POWERLINE_SEPARATOR_LEFT_THIN}" \
+		"date_day 4 0" \
+		"date 4 0 ${TMUX_POWERLINE_SEPARATOR_LEFT_THIN}" \
+		"time 4 0 ${TMUX_POWERLINE_SEPARATOR_LEFT_THIN}" \
 		#"utc_time 235 136 ${TMUX_POWERLINE_SEPARATOR_LEFT_THIN}" \
 	)
 fi
