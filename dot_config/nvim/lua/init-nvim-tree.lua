14a527518b29cb3ebc01d4a2b6b6e131cd6b362c
Quentin Bonnet <quentin.bonnet.work@gmail.com>
2024-01-15 01:36:37 +0100

    Fix nvim-tree to not get error on startup anymore; and revert to default keybindings for now.

--- dot_config/nvim/lua/init-nvim-tree.lua
+++ dot_config/nvim/lua/init-nvim-tree.lua
@@ -5,6 +5,34 @@ vim.g.loaded_netrwPlugin = 1
 -- empty setup using defaults
 -- require("nvim-tree").setup()
 
+local function my_on_attach(bufnr)
+  local api = require('nvim-tree.api')
+
+  local function opts(desc)
+    return { desc = 'nvim-tree: ' .. desc, buffer = bufnr, noremap = true, silent = true, nowait = true }
+  end
+
+  -- copy default mappings here from defaults in next section
+  api.config.mappings.default_on_attach(bufnr)
+  vim.keymap.del('q', { buffer = bufnr }) -- Disable the default quit shortcut, we wanna get command history
+  --TODO set those keys
+        -- { key = "u", action = "dir_up" },
+        -- { key = "n", action = "create" },
+        -- { key = "d", action = "trash" },
+        -- { key = "D", action = "remove" },
+  ---
+
+  -- remove a default
+  -- vim.keymap.del('n', '<C-]>', { buffer = bufnr })
+
+  -- override a default
+  -- vim.keymap.set('n', '<C-e>', api.tree.reload,                       opts('Refresh'))
+
+  -- add your mappings
+  -- vim.keymap.set('n', '?',     api.tree.toggle_help,                  opts('Help'))
+  ---
+end
+
 -- OR setup with some options
 require("nvim-tree").setup({
   sort_by = "case_sensitive",
@@ -16,6 +44,7 @@ require("nvim-tree").setup({
       },
     },
   },
+  on_attach = my_attach,
   view = {
     adaptive_size = true,
     -- centralize_selection = true,
@@ -23,15 +52,6 @@ require("nvim-tree").setup({
 		-- 	enable = true,
 		-- 	quit_on_focus_loss = true,
 		-- },
-    mappings = {
-      list = {
-        { key = "u", action = "dir_up" },
-        { key = "n", action = "create" },
-        { key = "d", action = "trash" },
-        { key = "D", action = "remove" },
-        { key = "q", action = "" }, -- Disable the default quit shortcut, we wanna get command history
-      },
-    },
   },
   renderer = {
     group_empty = true,
