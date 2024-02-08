ae96cfab1bf40fa026dbbab00feca9c7e2527925
Quentin Bonnet <quentin@bonnet.software>
2024-02-09 00:57:14 +0100

    Arandr layouts

---
+++ dot_screenlayout/private_executable_dual-screen-bureau.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+xrandr --output eDP-1 --mode 1366x768 --pos 0x672 --rotate normal --output DP-1 --off --output HDMI-1 --off --output HDMI-2 --primary --mode 2560x1440 --pos 1366x0 --rotate normal
