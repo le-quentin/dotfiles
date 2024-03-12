b7f63bf22c4127c4fa78264ef69bd752b8f74e33
Quentin Bonnet <quentin@bonnet.software>
2024-03-12 12:13:22 +0100

    [powerline] Python venv on the left segments, more obvious

--- dot_p10k.zsh
+++ dot_p10k.zsh
@@ -33,6 +33,7 @@
   typeset -g POWERLEVEL9K_LEFT_PROMPT_ELEMENTS=(
     # =========================[ Line #1 ]=========================
     os_icon                 # os identifier
+    virtualenv              # python virtualenv
     dir                     # current directory
     vcs                     # git status
     # =========================[ Line #2 ]=========================
@@ -51,7 +52,7 @@
     background_jobs         # presence of background jobs
     direnv                  # direnv status (https://direnv.net/)
     asdf                    # asdf version manager (https://github.com/asdf-vm/asdf)
-    virtualenv              # python virtual environment (https://docs.python.org/3/library/venv.html)
+    # virtualenv              # python virtual environment (https://docs.python.org/3/library/venv.html)
     anaconda                # conda environment (https://conda.io/)
     pyenv                   # python environment (https://github.com/pyenv/pyenv)
     goenv                   # go environment (https://github.com/syndbg/goenv)
