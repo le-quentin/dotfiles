478a3c5826d0616dd2d0bf607feeced11ec39740
Quentin Bonnet <quentin@bonnet.software>
2024-08-25 12:53:13 +0200

    [randr] Dual screen setup

--- dot_screenlayout/private_executable_dual-screen-bureau.sh
+++ dot_screenlayout/private_executable_dual-screen-bureau.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-xrandr --output eDP-1 --mode 1366x768 --pos 0x672 --rotate normal --output DP-1 --off --output HDMI-1 --off --output HDMI-2 --primary --mode 2560x1440 --pos 1366x0 --rotate normal
+xrandr --output eDP-1 --mode 1920x1080 --pos 0x662 --rotate normal --output DP-1 --off --output DP-2 --off --output DP-1-0 --off --output DP-1-1 --off --output DP-1-2 --off --output DP-1-3 --off --output HDMI-1-0 --primary --mode 2560x1440 --pos 1920x0 --rotate normal --output DP-1-4 --off --output DP-1-5 --off
