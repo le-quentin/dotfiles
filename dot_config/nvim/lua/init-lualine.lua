074703ffb3474aee70cb08a25c0843e974ad1e60
Quentin Bonnet <quentin@bonnet.software>
2026-05-22 13:18:08 +0200

    nvim: fix lualine by using default theme it actually knows

--- dot_config/nvim/lua/init-lualine.lua
+++ dot_config/nvim/lua/init-lualine.lua
@@ -2,7 +2,7 @@ require('lualine').setup {
   options = {
     icons_enabled = true,
     -- theme = 'pywal',
-    theme = "catppuccin",
+    theme = "nightfox",
     component_separators = { left = '', right = ''},
     section_separators = { left = '', right = ''},
     disabled_filetypes = {
